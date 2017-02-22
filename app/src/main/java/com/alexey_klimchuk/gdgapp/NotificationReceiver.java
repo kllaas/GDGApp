1b344bfea014c847f48df1036697d907d814b610
alexey <kll.a.s@yandex.ru>
2017-02-22 14:56:06 +0200

    show notification if user don`t open app 1 day

--- app/src/main/java/com/alexey_klimchuk/gdgapp/NotificationReceiver.java
+++ app/src/main/java/com/alexey_klimchuk/gdgapp/NotificationReceiver.java
@@ -1,14 +1,10 @@
 package com.alexey_klimchuk.gdgapp;
 
-import android.app.ActivityManager;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
-
-import java.util.List;
 
 public class NotificationReceiver extends BroadcastReceiver {
 
@@ -18,23 +14,10 @@ public class NotificationReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        if (!isRunningActivities(context)) {
-            NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-            Notification notification = intent.getParcelableExtra(NOTIFICATION);
-            int id = intent.getIntExtra(NOTIFICATION_ID, 0);
-            manager.notify(id, notification);
-        }
-        Log.d("notification", isRunningActivities(context) + "");
-    }
-
-    public boolean isRunningActivities(Context context) {
-
-        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-        List<ActivityManager.RunningTaskInfo> services = activityManager
-                .getRunningTasks(Integer.MAX_VALUE);
-
-        return services.get(0).topActivity.getPackageName()
-                .equalsIgnoreCase(context.getPackageName());
+        Notification notification = intent.getParcelableExtra(NOTIFICATION);
+        int id = intent.getIntExtra(NOTIFICATION_ID, 0);
+        manager.notify(id, notification);
     }
 }
